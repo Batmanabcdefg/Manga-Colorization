52068c533df927c75480adc74c3a931a42ce147f
neil <nilabja10201992@gmail.com>
2019-03-21 22:00:06 +0530

    leak proofing

--- level_set.py
+++ level_set.py
@@ -7,7 +7,7 @@ import numpy as np
 
 class drlse(object):
 
-	def __init__(self, F, lamda, mu, alpha, epsilon, dt, iterations, potential_function):
+	def __init__(self, F, lamda, mu, alpha, epsilon, dt, iterations, potential_function, M1, M2, F1):
 		self.F = F
 		self.lamda = lamda
 		self.alpha = alpha
@@ -16,6 +16,12 @@ class drlse(object):
 		self.mu = mu
 		self.iter = iterations
 		self.potential_function = potential_function
+		self.M1 = M1
+		self.M2 = M2
+		self.F1 =F1
+
+	def sigmoid(self, x):
+		return np.exp(x)/(1+ np.exp(x))
 
 	def drlse_edge(self,phi):
 		[vy, vx] = np.gradient(self.F)
@@ -35,8 +41,10 @@ class drlse(object):
 		        print('Error: Wrong choice of potential function. Please input the string "single-well" or "double-well" in the drlse_edge function.')
 		    diracPhi = self.Dirac(phi)
 		    areaTerm = diracPhi * self.F
-		    edgeTerm = diracPhi * (vx * Nx + vy * Ny) + diracPhi * self.F * curvature
-		    phi = phi + self.dt * (self.mu * distRegTerm + self.lamda * edgeTerm + self.alpha * areaTerm)
+		    edgeTerm = diracPhi * (vx * Nx + vy * Ny) + diracPhi*self.F * curvature
+		    x = (self.F1 - self.M2)/(self.M1 - self.M2)
+		    leakproofterm = self.F*areaTerm*self.sigmoid(x)
+		    phi = phi + self.dt * (self.mu * distRegTerm + self.lamda * edgeTerm + self.alpha * areaTerm - leakproofterm*self.alpha)
 		return phi
 
 	def distReg_p2(self,phi):
@@ -107,16 +115,21 @@ class levelSet(object):
 		img_smooth = filters.gaussian_filter(image, self.sigma)
 		[Iy, Ix] = np.gradient(img_smooth)
 		f = np.square(Ix) + np.square(Iy)
-		return 1 / (1+f)
+		f1 = np.sqrt(f)
+		print(np.unique(f))
+		return 1 / (1+f), np.max(f1), np.min(f1), f1
 
 	def gradientDescent(self,image,x,y):
 		phi = self.initializePhiAtScribble(image,x,y)
-		F = self.calculateF(image)
-		lse = drlse(F, self.lamda, self.mu, self.alpha, self.epsilon, self.dt, self.drlse_iter, self.potential_function)
+		F, M1, M2, F1 = self.calculateF(image)
+		lse = drlse(F, self.lamda, self.mu, self.alpha, self.epsilon, self.dt, self.drlse_iter, self.potential_function, M1, M2, F1)
 		for n in range(self.gradient_iter):
 			phi = lse.drlse_edge(phi)
+			#print(np.unique(phi))
+			#plt.show()
 			if np.mod(n, 20) == 0:			
 				self.visualization(image,phi)
+				print(n)
 				plt.pause(0.3)
 		plt.pause(5)
 
@@ -125,9 +138,9 @@ def main():
 	# iter_inner, iter_outer, lamda, alpha, epsilon, sigma, dt, potential_function
 	# potential_function="single-well"
 
-	image = imread('1.png', True)
+	image = imread('123.png', True)
 	image = np.array(image,dtype='float32')
-	LS = levelSet(4,101,2,-9,2.0,0.8)
+	LS = levelSet(4,101,2,-0.1,2.0,0.8)
 	LS.gradientDescent(image,240,150)
 
 
