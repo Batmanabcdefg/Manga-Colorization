b5277b9438bf11f85464be83dae58dd3bf0c629d
Arpit Gupta <arpit.g@students.iiit.ac.in>
2019-03-21 22:08:37 +0530

    Color filling for intensity continuos region

--- level_set.py
+++ level_set.py
@@ -1,13 +1,17 @@
 from scipy.misc import imread
+from skimage.color import rgb2gray
 import matplotlib.pyplot as plt
+import webcolors
 from mpl_toolkits.mplot3d import Axes3D
 import scipy.ndimage.filters as filters
 from skimage import measure
+import cv2
+# import drlse_algo as drlse
 import numpy as np
 
 class drlse(object):
 
-	def __init__(self, F, lamda, mu, alpha, epsilon, dt, iterations, potential_function, M1, M2, F1):
+	def __init__(self, F, lamda, mu, alpha, epsilon, dt, iterations, potential_function):
 		self.F = F
 		self.lamda = lamda
 		self.alpha = alpha
@@ -16,22 +20,19 @@ class drlse(object):
 		self.mu = mu
 		self.iter = iterations
 		self.potential_function = potential_function
-		self.M1 = M1
-		self.M2 = M2
-		self.F1 =F1
-
-	def sigmoid(self, x):
-		return np.exp(x)/(1+ np.exp(x))
 
 	def drlse_edge(self,phi):
 		[vy, vx] = np.gradient(self.F)
+		# [vy, vx, vz] = np.gradient(self.F)
 		for k in range(self.iter):
 		    phi = self.applyNeumann(phi)
 		    [phi_y, phi_x] = np.gradient(phi)
+		    # [phi_y, phi_x, phi_z] = np.gradient(phi)
 		    s = np.sqrt(np.square(phi_x) + np.square(phi_y))
 		    smallNumber = 1e-10
 		    Nx = phi_x / (s + smallNumber)
 		    Ny = phi_y / (s + smallNumber)
+		    # Nz = phi_z / (s + smallNumber)
 		    curvature = self.div(Nx, Ny)
 		    if self.potential_function == 'single-well':
 		        distRegTerm = filters.laplace(phi, mode='wrap') - curvature
@@ -41,14 +42,13 @@ class drlse(object):
 		        print('Error: Wrong choice of potential function. Please input the string "single-well" or "double-well" in the drlse_edge function.')
 		    diracPhi = self.Dirac(phi)
 		    areaTerm = diracPhi * self.F
-		    edgeTerm = diracPhi * (vx * Nx + vy * Ny) + diracPhi*self.F * curvature
-		    x = (self.F1 - self.M2)/(self.M1 - self.M2)
-		    leakproofterm = self.F*areaTerm*self.sigmoid(x)
-		    phi = phi + self.dt * (self.mu * distRegTerm + self.lamda * edgeTerm + self.alpha * areaTerm - leakproofterm*self.alpha)
+		    edgeTerm = diracPhi * (vx * Nx + vy * Ny) + diracPhi * self.F * curvature
+		    phi = phi + self.dt * (self.mu * distRegTerm + self.lamda * edgeTerm + self.alpha * areaTerm)
 		return phi
 
 	def distReg_p2(self,phi):
 	    [phi_y, phi_x] = np.gradient(phi)
+	    # [phi_y, phi_x, phi_z] = np.gradient(phi)
 	    s = np.sqrt(np.square(phi_x) + np.square(phi_y))
 	    a = (s >= 0) & (s <= 1)
 	    b = (s > 1)
@@ -59,6 +59,8 @@ class drlse(object):
 	def div(self,nx, ny):
 	    [junk, nxx] = np.gradient(nx)
 	    [nyy, junk] = np.gradient(ny)
+	    # [junk, nxx, nzz] = np.gradient(nx)
+	    # [nyy, junk, nzz] = np.gradient(ny)
 	    return nxx + nyy
 
 	def Dirac(self,x):
@@ -68,6 +70,7 @@ class drlse(object):
 
 	def applyNeumann(self,f):
 	    [ny, nx] = f.shape
+	    # [ny, nx, nz] = f.shape
 	    g = f.copy()
 	    g[0, 0] = g[2, 2]
 	    g[0, nx-1] = g[2, nx-3]
@@ -103,45 +106,63 @@ class levelSet(object):
 		return phi
 
 	def visualization(self,image,phi):
+		contours = measure.find_contours(phi, 0)
 		fig2 = plt.figure(2)
 		fig2.clf()
-		contours = measure.find_contours(phi, 0)
 		ax2 = fig2.add_subplot(111)
 		ax2.imshow(image, interpolation='nearest', cmap=plt.cm.gray)
 		for n, contour in enumerate(contours):
 			ax2.plot(contour[:, 1], contour[:, 0], linewidth=2)
+		return contour
 
 	def calculateF(self,image):
 		img_smooth = filters.gaussian_filter(image, self.sigma)
 		[Iy, Ix] = np.gradient(img_smooth)
 		f = np.square(Ix) + np.square(Iy)
-		f1 = np.sqrt(f)
-		print(np.unique(f))
-		return 1 / (1+f), np.max(f1), np.min(f1), f1
+		# [Iy, Ix, Iz] = np.gradient(img_smooth)
+		# f = np.square(Ix) + np.square(Iy) + np.square(Iz)
+		return 1 / (1+f)
+
+	def fillColor(self,image,boundary,rgb):
+		boundary[:,[0,1]] = boundary[:,[1,0]]
+		# rgb = (webcolors.name_to_rgb(col)[0],webcolors.name_to_rgb(col)[1],webcolors.name_to_rgb(col)[2])
+		# print(rgb)
+		for x in range(image.shape[0]):
+			for y in range(image.shape[1]):
+				if(cv2.pointPolygonTest(boundary,(x,y),True)>0):
+					image[y][x][0] = rgb[0]
+					image[y][x][1] = rgb[1]
+					image[y][x][2] = rgb[2]
+		# cv2.fillPoly(image, pts =[boundary], color=rgb)
+		cv2.imwrite("123.png",image)
+		cv2.imshow("sdjkfnskj",image)
+		cv2.waitKey(0)
 
 	def gradientDescent(self,image,x,y):
 		phi = self.initializePhiAtScribble(image,x,y)
-		F, M1, M2, F1 = self.calculateF(image)
-		lse = drlse(F, self.lamda, self.mu, self.alpha, self.epsilon, self.dt, self.drlse_iter, self.potential_function, M1, M2, F1)
+		F = self.calculateF(image)
+		lse = drlse(F, self.lamda, self.mu, self.alpha, self.epsilon, self.dt, self.drlse_iter, self.potential_function)
 		for n in range(self.gradient_iter):
 			phi = lse.drlse_edge(phi)
-			#print(np.unique(phi))
-			#plt.show()
-			if np.mod(n, 20) == 0:			
-				self.visualization(image,phi)
-				print(n)
+			if np.mod(n, 2) == 0:			
+				boundary = self.visualization(image.copy(),phi)
 				plt.pause(0.3)
-		plt.pause(5)
+		return np.int32(boundary)
+		# plt.pause(5)
 
 def main():
 
 	# iter_inner, iter_outer, lamda, alpha, epsilon, sigma, dt, potential_function
 	# potential_function="single-well"
 
-	image = imread('123.png', True)
+	image = cv2.imread('1.png',True)
+	# plt.imshow(image)
+	# plt.show()
+	# print(image.shape)
 	image = np.array(image,dtype='float32')
-	LS = levelSet(4,101,2,-0.1,2.0,0.8)
-	LS.gradientDescent(image,240,150)
+	LS = levelSet(4,100,2,-9,2.0,0.8)
+	boundary = LS.gradientDescent(image[:,:,0],236,170)
+	LS.fillColor(image,boundary,(0,0,255))
 
 
 if __name__ == '__main__':
